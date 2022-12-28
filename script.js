1185cf5614a52dcc6acd9b720e57f5002f0413bc
berger <robertberger.dev@gmail.com>
2022-12-28 17:50:13 -0500

    Added 3rd Promise

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ const L05Promise2 = new Promise((resolve, reject) => {
 });
 
 L05Promise2.then(result => console.log(result));
+
+const L05Promise3 = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        reject("Something went wrong");
+    }, 4000);
+});
+
+L05Promise3.catch(err => console.log(err));
