c5870117d52a13b133784f399c719aa8f01a64f3
berger <robertberger.dev@gmail.com>
2022-12-28 17:44:37 -0500

    Added Second Promise

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const L05Promise = new Promise((resolve, reject) => {
 });
 
 L05Promise.then(result => console.log(result));
+
+const L05Promise2 = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        resolve("Hello? Can you hear me?");
+    }, 3000);
+});
+
+L05Promise2.then(result => console.log(result));
